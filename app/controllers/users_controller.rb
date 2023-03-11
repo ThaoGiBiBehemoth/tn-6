b6d7bdfdc668214cc98384529ab84547b288598c
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-03-12 02:26:03 +0700

    8th: controller_user_action_show_update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  before_action :authorize, only: [:show, :update]
+  before_action :set_user, only: [:show, :update]
+   
+  # LIST: cái này có thể dùng cho admin về sau, pj này ko cần, để đây thôi =))
+  # def index
+  #   @users = User.all
+  #   render json: @users
+  # end
+
+  # SHOW INFO (GET /users/1) 
+  def show
+    render json: @user
+  end
+
+  # UPDATE (PATCH/PUT /users/1)
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+
+  # SIGNUP/ REGISTER
   def create
     @user = User.create(user_params)
     if @user.valid?
@@ -9,6 +33,7 @@ class UsersController < ApplicationController
     end
   end
 
+  # LOGIN
   def login
     @user = User.find_by(nickname: user_params[:nickname])
     if @user && @user.authenticate(user_params[:password])
@@ -22,5 +47,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:nickname, :password, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
